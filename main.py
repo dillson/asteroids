cf64967c64ab4ac6cd74298a232e2b2f54f1d14e
dan_illson <illson.dan@gmail.com>
2024-09-15 13:38:03 -0600

    ch 1 and 2 exercises

--- main.py
+++ main.py
@@ -1,21 +1,37 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
-
+    game_clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
+    
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        for obj in updateable:
+            obj.update(dt)
+
         screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+        
         pygame.display.flip()
 
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        dt = game_clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
